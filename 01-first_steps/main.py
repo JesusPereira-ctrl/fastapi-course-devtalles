3268cb4cb7786ad8ea3ee51e590d16ccb7396651
Jesus Pereira <jesuspereirap100@gmail.com>
2026-06-11 18:00:47 -0400

    last commit xd

--- 01-first_steps/main.py
+++ 01-first_steps/main.py
@@ -1,9 +1,123 @@
 
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, HTTPException, Body, Query
+from pydantic import BaseModel, Field
 
 app = FastAPI(title="Mini Blog")
+
+BLOG_POST = [
+    {
+        "id": 1,
+        "title": "Hola desde FastAPI",
+        "content": "Mi primer post con FastAPI"
+    },
+    {
+        "id": 2,
+        "title": "Mi segundo Post con FastAPI",
+        "content": "Mi segundo post con FastAPI blablabla"
+    },
+    {
+        "id": 3,
+        "title": "Django vs FastAPI",
+        "content": "FastAPI es más rápido por x razones"
+    }
+]
+
+
+class PostBase(BaseModel):
+    title: str
+    content: Optional[str] = "Contenido no disponible"
+
+
+class PostCreate(BaseModel):
+    title: str = Field(
+        ...,
+        min_length=3,
+        max_length=100,
+        description="Titulo del post (mínimo 3 caracteres, máximo 100)",
+        examples=["Mi primer post con FastAPI"]
+    )
+    content: Optional[str] = Field(
+        default="Contenido no disponible",
+        min_length=10,
+        description="Contenido del post (mínimo 10 caracteres)",
+        examples=["Este es un contenido válido porque tiene 10 caracteres o más"]
+    )
+
+
+class PostUpdate(BaseModel):
+    title: str
+    content: Optional[str] = None
 
 
 @app.get("/")
 def home():
     return {"message": "Bienvenidos a Mini Blog por Devtalles"}
+
+
+@app.get("/posts")
+def list_posts(query: str | None = Query(default=None, description="Texto para buscar por título")):
+
+    if query:
+        results = [
+            post for post in BLOG_POST
+            if query.lower() in post["title"].lower()
+        ]
+
+        return {"data": results, "query": query}
+
+    return {"data": BLOG_POST}
+
+
+@app.get("/posts/{post_id}")
+def get_post(
+    post_id: int,
+    include_content: bool = Query(
+        default=True, description="Incluir o no el contenido")
+):
+    for post in BLOG_POST:
+        if post["id"] == post_id:
+            if not include_content:
+                return {"id": post["id"], "title": post["title"]}
+            return {"data": post}
+
+    return {"error": "Post no encontrado"}
+
+
+@app.post("/posts")
+def create_post(post: PostCreate):
+    new_id = (BLOG_POST[-1]["id"] + 1) if BLOG_POST else 1
+    new_post = {
+        "id": new_id,
+        "title": post.title,
+        "content": post.content
+    }
+    BLOG_POST.append(new_post)
+    return {"message": "Post creado", "data": new_post}
+
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, data: PostUpdate):
+    for post in BLOG_POST:
+        if post["id"] == post_id:
+            payload = data.model_dump(
+                exclude_unset=True)
+            # {"title": "Ricardo"} si fuera unset
+            # {"title": "Ricardo", "content": None} si no fuera unset
+            if "title" in payload:
+                post["title"] = payload["title"]
+            if "content" in payload:
+                post["content"] = payload["content"]
+            return {"message": "Post actualizado", "data": post}
+
+    raise HTTPException(status_code=404, detail="Post no encontrado")
+
+
+@app.delete("/posts/{post_id}", status_code=204)
+def delete_post(post_id: int):
+    for index, post in enumerate(BLOG_POST):
+        if post["id"] == post_id:
+            BLOG_POST.pop(index)
+            return
+    raise HTTPException(status_code=404, detail="Post no encontrado")
